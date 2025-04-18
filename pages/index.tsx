3e9873525003b5fa7eea6cdfb30ef80dcae2c7ae
williamk19 <williamkurniawan1144@gmail.com>
2025-04-18 19:58:06 +0700

    fix some types error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,31 @@
 import Box from "@/src/components/Box";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import Head from "next/head";
+import { useState } from "react";
 
 export default function Home() {
+	const [rotate, setRotate] = useState(true);
+
 	return (
 		<>
 			<Head>
 				<title>Learn React Three Fiber</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
+			<div style={{ margin: "1rem" }}>
+                <button onClick={() => setRotate((r) => !r)}>
+                    {rotate ? "Stop Rotation" : "Start Rotation"}
+                </button>
+            </div>
 			<div className="">
-				<Canvas style={{ height: "100vh" }}>
+				<Canvas style={{ height: "90vh" }}>
 					<ambientLight intensity={Math.PI / 2} />
 					<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
 					<pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-					<Box position={[-1.2, 0, 0]} />
-					<Box position={[1.2, 0, 0]} />
+					<Box position={[-1.2, 0, 0]} rotate={rotate} />
+					<Box position={[1.2, 0, 0]} rotate={rotate} />
+					<OrbitControls />
 				</Canvas>
 			</div>
 		</>
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,24 +1,37 @@
 import { useFrame } from "@react-three/fiber"
 import { useRef, useState } from "react"
+import { Mesh } from "three"
 
-const Box = (props: any) => {
-	const meshRef = useRef()
-	const [hovered, setHover] = useState(false)
-	const [active, setActive] = useState(false)
-	useFrame((state, delta) => (meshRef.current.rotation.x += delta))
+interface BoxProps {
+    rotate?: boolean;
+    [key: string]: any;
+}
 
-	return (
-		<mesh
-			{...props}
-			ref={meshRef}
-			scale={active ? 1.5 : 1}
-			onClick={(event: any) => setActive(!active)}
-			onPointerOver={(event: any) => setHover(true)}
-			onPointerOut={(event: any) => setHover(false)}>
-			<boxGeometry args={[1, 1, 1]} />
-			<meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-		</mesh>
-	)
+const Box = ({
+	rotate = true,
+	...props
+}: BoxProps) => {
+    const meshRef = useRef<Mesh>(null)
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    useFrame((state, delta) => {
+        if (rotate && meshRef.current) {
+            meshRef.current.rotation.x += delta
+        }
+    })
+
+    return (
+        <mesh
+            {...props}
+            ref={meshRef}
+            scale={active ? 1.5 : 1}
+            onClick={(event: any) => setActive(!active)}
+            onPointerOver={(event: any) => setHover(true)}
+            onPointerOut={(event: any) => setHover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
 }
 
 export default Box;
